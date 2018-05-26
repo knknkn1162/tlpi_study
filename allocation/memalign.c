42d5bb64e08460887f448aae4c7225f2e9cd0a32
knknkn <knknkn1162@gmail.com>
2018-05-26 19:21:46 +0900

    add comment on memalign.c

--- allocation/memalign.c
+++ allocation/memalign.c
@@ -5,6 +5,7 @@ int main(void) {
   int s;
   void *memptr;
 
+  // `memalign` doesn't exist mac OS. Use `posix_memalign` instead.
   s = posix_memalign(&memptr, 1024 * sizeof(void *), 65536);
   if (s != 0) {
     errExit("posix_memalign");
