a02fd733d47b152f53dc7799f99e5232820c9f7e
knknkn <knknkn1162@gmail.com>
2018-05-26 17:56:04 +0900

    add comment on allocate_twice

--- allocation/allocate_twice.c
+++ allocation/allocate_twice.c
@@ -15,4 +15,10 @@ int main(void) {
   // same as initial program break
   printf("After program break: %10p\n", sbrk(0));
   free(str);
+  // The error occurs when invoking free more than once!
+  // res(10084,0x7fff8e5e1340) malloc: *** error for object 0x10c26f000: pointer
+  // being freed was not allocated
+  // *** set a breakpoint in malloc_error_break to debug */
+  // zsh: abort      ./res
+  // free(str);
 }
