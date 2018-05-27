5808eb27a243a3afaf7d5ace4ba1670359b50b81
knknkn <knknkn1162@gmail.com>
2018-05-27 12:04:00 +0900

    try time system call

--- time/base.c
+++ time/base.c
@@ -2,14 +2,23 @@
 #include <sys/time.h>
 #include <time.h>
 
+#define TIME_SIZE 100
+
 int main(int argc, char *argv[]) {
   struct timeval tv;
-  int res;
   time_t tm;
-  res = gettimeofday(&tv, NULL);
-
+  char res = gettimeofday(&tv, NULL);
+  struct tm *localtm;
   tm = time(NULL);
+
   printf("tv_sec: %ld, tv_usec: %d => return : %d\n", tv.tv_sec, tv.tv_usec,
          res);
   printf("time: %ld\n", tm);
+  printf("ctime: %s\n", ctime(&tm));
+
+  localtm = localtime(&tm);
+  printf("%d:%d:%d\n", localtm->tm_hour, localtm->tm_min, localtm->tm_sec);
+
+  tm = mktime(localtm);
+  printf("%ld\n", tm);
 }
